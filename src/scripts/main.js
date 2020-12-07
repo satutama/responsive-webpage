3fe57c724e8b770801831488e94154f827e660bc
Utama <satria.utama@glencore.com>
2020-12-07 10:56:39 +0100

    added pop up modal

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,3 +22,21 @@ for (var i = 0; i < faqs.length; i++) {
     e.preventDefault();
     });
 }; 
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
